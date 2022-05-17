5cdf41b1fccf7d02985f4482795368ba88c60a71
sourav pahwa <Sourav61pahwa@gmail.com>
2022-05-18 02:17:19 +0530

    added gitignore file
    added gitignore file

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
           </ul>
           <form className="form-inline my-2 my-lg-0">
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Sign In</button>
           </form>
         </div>
       </nav>
